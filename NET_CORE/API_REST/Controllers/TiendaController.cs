5784951fc0493e920b7c2c737680cc85221932b6
afa001 <anderson.alarcon.afa@gmail.com>
2020-07-20 16:47:18 -0500

    se agregaron los controladores de la api

---
+++ NET_CORE/API_REST/Controllers/TiendaController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_REST.Models;
+
+namespace API_REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TiendaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TiendaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tienda
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tienda>>> GetTienda()
+        {
+            return await _context.Tienda.ToListAsync();
+        }
+
+        // GET: api/Tienda/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tienda>> GetTienda(int id)
+        {
+            var tienda = await _context.Tienda.FindAsync(id);
+
+            if (tienda == null)
+            {
+                return NotFound();
+            }
+
+            return tienda;
+        }
+
+        // PUT: api/Tienda/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTienda(int id, Tienda tienda)
+        {
+            if (id != tienda.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tienda).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TiendaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tienda
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Tienda>> PostTienda(Tienda tienda)
+        {
+            _context.Tienda.Add(tienda);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTienda", new { id = tienda.Id }, tienda);
+        }
+
+        // DELETE: api/Tienda/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Tienda>> DeleteTienda(int id)
+        {
+            var tienda = await _context.Tienda.FindAsync(id);
+            if (tienda == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tienda.Remove(tienda);
+            await _context.SaveChangesAsync();
+
+            return tienda;
+        }
+
+        private bool TiendaExists(int id)
+        {
+            return _context.Tienda.Any(e => e.Id == id);
+        }
+    }
+}
